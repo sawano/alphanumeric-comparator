9aebababc28bb5f5bcfa85cb6148322faf8f358d
Daniel Sawano <daniel@sawano.se>
2014-10-27 21:53:36 +0100

    Renamed tests

--- src/test/java/se/sawano/java/text/AlphanumericComparatorTest.java
+++ src/test/java/se/sawano/java/text/AlphanumericComparatorTest.java
@@ -204,60 +204,88 @@
 
 package se.sawano.java.text;
 
-import org.apache.commons.io.IOUtils;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-import java.io.IOException;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Locale;
 
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 
+@RunWith(Parameterized.class)
 public class AlphanumericComparatorTest {
 
-    @Test
-    public void should_demonstrate_sorting() throws Exception {
-        givenStrings("HD 20GB", "HD 2GB");
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return asList(new Object[][]{
 
-        whenSorting();
+                {givenComparator(), whenSorting("image02.jpg", "image1.jpg"), thenOrderShouldBe("image1.jpg", "image02.jpg")},
+                {givenComparator(), whenSorting("Hard drive 20GB", "Hard drive 2GB"), thenOrderShouldBe("Hard drive 2GB", "Hard drive 20GB")},
+                {givenComparator(), whenSorting("iPhone", "jPhone", "IPhone"), thenOrderShouldBe("IPhone", "iPhone", "jPhone")},
+                {givenSwedishComparator(), whenSorting("iPhone", "jPhone", "IPhone"), thenOrderShouldBe("iPhone", "IPhone", "jPhone")},
+                {givenComparator(), whenSorting("A", "a"), thenOrderShouldBe("A", "a")},
+                {givenComparator(), whenSorting("a", "a"), thenOrderShouldBe("a", "a")},
+                {givenSwedishComparator(), whenSorting("A", "a"), thenOrderShouldBe("a", "A")},
+                {givenSwedishComparator(), whenSorting("a", "a"), thenOrderShouldBe("a", "a")},
+                {givenSwedishComparator(), whenSorting("ä", "a"), thenOrderShouldBe("a", "ä")},
+                {givenDanishComparator(), whenSorting("A", "a"), thenOrderShouldBe("A", "a")},
+                {givenFrenchComparator(), whenSorting("A", "a"), thenOrderShouldBe("a", "A")},
+                {givenComparator(), whenSorting("ä", "b"), thenOrderShouldBe("b", "ä")},
+                {givenGermanComparator(), whenSorting("ä", "b"), thenOrderShouldBe("ä", "b")},
+                {givenComparator(), whenSorting("10", "1", "-1", "-a", "20", "02", "a"), thenOrderShouldBe("-1", "-a", "1", "02", "10", "20", "a")},
+                {givenComparator(), whenSorting("", ""), thenOrderShouldBe("", "")},
+                {givenComparator(), whenSorting("0", "0text"), thenOrderShouldBe("0", "0text")},
+                {givenComparator(), whenSorting("text0", "text10"), thenOrderShouldBe("text0", "text10")},
+                {givenComparator(), whenSorting("some text", "some other text"), thenOrderShouldBe("some other text", "some text")}
 
-        thenOrderShouldBe("HD 2GB", "HD 20GB");
+        });
+    }
+
+    private static List<String> thenOrderShouldBe(final String... strings) {
+        return asList(strings);
+    }
+
+    private static List<String> whenSorting(final String... strings) {
+        return asList(strings);
+    }
+
+    private static AlphanumericComparator givenSwedishComparator() {
+        return new AlphanumericComparator(new Locale("sv", "SE"));
+    }
+
+    private static AlphanumericComparator givenDanishComparator() {
+        return new AlphanumericComparator(new Locale("da", "DK"));
+    }
+
+    private static AlphanumericComparator givenFrenchComparator() {
+        return new AlphanumericComparator(Locale.FRENCH);
+    }
+
+    private static AlphanumericComparator givenGermanComparator() {
+        return new AlphanumericComparator(Locale.GERMAN);
+    }
+
+    private static AlphanumericComparator givenComparator() {
+        return new AlphanumericComparator();
+    }
+
+    private final Comparator<String> comparator;
+    private final List<String> unsorted;
+    private final List<String> expectedResult;
+
+    public AlphanumericComparatorTest(final Comparator<String> comparator, final List<String> unsorted, final List<String> expectedResult) {
+        this.comparator = comparator;
+        this.unsorted = unsorted;
+        this.expectedResult = expectedResult;
     }
 
     @Test
-    public void should_sort_file_names() throws Exception {
-        givenStrings(readLines("files_unsorted.txt"));
-
-        whenSorting();
-
-        thenOrderShouldBe(readLines("files_sorted.txt"));
+    public void should_sort_correctly() throws Exception {
+        unsorted.sort(comparator);
+        assertEquals(expectedResult, unsorted);
     }
-
-    private void whenSorting() {
-        stringsToSort.sort(new AlphanumericComparator());
-    }
-
-    @SuppressWarnings("unchecked")
-    private List<String> readLines(final String fileName) throws IOException {
-        return IOUtils.readLines(getClass().getResourceAsStream("/" + fileName));
-    }
-
-    private List<String> stringsToSort;
-
-    private void thenOrderShouldBe(final String... expected) {
-        thenOrderShouldBe(asList(expected));
-    }
-
-    private void thenOrderShouldBe(final List<String> expected) {
-        assertEquals(expected, stringsToSort);
-    }
-
-    private void givenStrings(final String... strings) {
-        stringsToSort = asList(strings);
-    }
-
-    private void givenStrings(final List<String> strings) {
-        stringsToSort = strings;
-    }
-
 }
